ed144357ac6443589a16069181241d98d6112d5e
alexsokolov <frank9154@gmail.com>
2023-11-19 17:30:37 +0300

    ThirdCommit

--- Lesson 2.04/ViewController.swift
+++ Lesson 2.04/ViewController.swift
@@ -8,22 +8,42 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
+    
     @IBOutlet var mainLabel: UILabel!
     @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var slider: UISlider!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: false)
-        mainLabel.text = "Main label"
+        //setupMainLabel()
+        setupSlider()
+        setupMainLabel()
+    }
+    
+    @IBAction func segmentedControlAction() {
+    }
+    
+    @IBAction func sliderAction() {
+        
+    }
+    
+    private func setupMainLabel() {
+        mainLabel.text = slider.value.formatted()
         mainLabel.font = .systemFont(ofSize: 35)
         mainLabel.textAlignment = .center
         mainLabel.numberOfLines = 2
     }
-
-    @IBAction func segmentedControlAction() {
-    }
     
+    private func setupSlider() {
+        slider.value = 1
+        slider.minimumValue = 1
+        slider.maximumValue = 100
+        slider.minimumTrackTintColor = .yellow
+        slider.maximumTrackTintColor = .red
+        slider.thumbTintColor = .green
+    }
 }
 
