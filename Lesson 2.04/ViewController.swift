5cd7d58692683bd601c6c03aa83aea57e8584061
alexsokolov <frank9154@gmail.com>
2023-11-19 20:43:00 +0300

    Final Commit

--- Lesson 2.04/ViewController.swift
+++ Lesson 2.04/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 final class ViewController: UIViewController {
     
+    // MARK: - IB Outlets
     @IBOutlet var mainLabel: UILabel!
     @IBOutlet var segmentedControl: UISegmentedControl!
     @IBOutlet var slider: UISlider!
+    @IBOutlet var textField: UITextField!
     
-    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,6 +25,7 @@ final class ViewController: UIViewController {
         setupMainLabel()
     }
     
+    // MARK: - IB Actions
     @IBAction func segmentedControlAction() {
         switch segmentedControl.selectedSegmentIndex {
         case 0:
@@ -45,6 +48,49 @@ final class ViewController: UIViewController {
         //view.alpha = CGFloat(slider.value)
     }
     
+    @IBAction func doneButtonAction (){
+        guard let inputText = textField.text, !inputText.isEmpty else {
+            //print("Text field is empty")
+            showAlert(withTitle: "Text field is empty", andMessage: "Please enter your name")
+            // выход из метода через ретерн
+            return
+        }
+        
+         /*if let _ = Double(inputText) {
+            //print("Wrong format")
+            showAlert(withTitle: "Wrong format", andMessage: "Please enter your name")
+            return
+        }
+          */
+        //задаем регулярное выражение
+        let usernamePattern = "^[a-zA-Z ]{3,20}$"
+        let isUsernameValid = NSPredicate(format: "SELF MATCHES %@", usernamePattern).evaluate(with: inputText)
+        
+        if !isUsernameValid {
+            showAlert(withTitle: "Wrong format", andMessage: "Please enter your name")
+            return
+        }
+        
+        
+        //передача из текстового поля в поле лейбл
+        mainLabel.text = inputText
+        textField.text = ""
+    }
+    
+    // MARK: - Private Methods
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.textField.text = ""
+        }
+    
+        // Далее добавляем кнопку в алерт контроллер
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+// MARK: - Setup UI
+extension ViewController {
     private func setupMainLabel() {
         mainLabel.text = slider.value.formatted()
         mainLabel.font = .systemFont(ofSize: 35)
@@ -54,11 +100,10 @@ final class ViewController: UIViewController {
     
     private func setupSlider() {
         slider.value = 1
-        slider.minimumValue = 1
-        slider.maximumValue = 100
+        slider.minimumValue = 0
+        slider.maximumValue = 1
         slider.minimumTrackTintColor = .yellow
         slider.maximumTrackTintColor = .red
         slider.thumbTintColor = .green
     }
 }
-
